95cf27556d225101aa16873807d5e72566be5510
Davi Goes <davi.vasconcelos@ucsal.edu.br>
2025-09-02 22:44:46 -0300

    Refatoraçao LSP

---
+++ src/main/java/br/com/mariojp/solid/lspaccounts/Withdrawable.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+package br.com.mariojp.solid.lspaccounts;
+
+
+interface Withdrawable {
+  void withdraw(double amount);
+}
